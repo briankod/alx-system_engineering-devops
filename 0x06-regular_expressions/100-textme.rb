f647c9d9428aa8e524ae42f6d1bdf33b9bd78be0
briankod <brianmugambi199@gmail.com>
2022-07-27 00:23:08 +0300

    Regular expression

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -6,6 +6,6 @@
 # The flags that were used
 
 sender = ARGV[0].scan(/(?<=\[from:)[^ ]+(?=\])/).join
-reciever = ARGV[0].scan(/(?<=\[to:)[^ ]+(?=\])/).join
+receiver = ARGV[0].scan(/(?<=\[to:)[^ ]+(?=\])/).join
 flags = ARGV[0].scan(/(?<=\[flags:)[^ ]+(?=\])/).join
-puts "#{sender},#{reciever},#{flags}"
+puts "#{sender},#{receiver},#{flags}"
