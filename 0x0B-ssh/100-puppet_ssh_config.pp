d36013662b6e6211c2b65d4cecf618e97ba727f6
briankod <brianmugambi199@gmail.com>
2022-09-19 03:01:05 +0300

    SSH

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# make changes to our configuration file (Using Puppet)
+
+include stdlib
+file_line { 'specify Identity File':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+}
+
+file_line { 'refuse password authenticate':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
+}
