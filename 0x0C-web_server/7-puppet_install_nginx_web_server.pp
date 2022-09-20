b47c23abfd23eb319b92589eb16cf339ec4ad37e
briankod <brianmugambi199@gmail.com>
2022-09-20 23:29:02 +0000

    Web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,10 @@
+# Configure an Ubuntu machine (Using Puppet)
+
+exec {'/usr/bin/env sudo apt update': }
+exec {'/usr/bin/env sudo apt install nginx': }
+exec {'/usr/bin/env sudo service nginx start': }
+exec {'/usr/bin/env echo "echo "Hello World!" | sudo tee -a /var/www/html/index.nginx-debian.html': }
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.holbertonschool.com permanent;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env echo "Ceci n'est pas une page" | sudo tee -a /var/www/html/custom_404.html': }
+exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env sudo service nginx restart': }
