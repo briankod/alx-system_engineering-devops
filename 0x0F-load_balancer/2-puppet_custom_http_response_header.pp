1bef9cb2a13ec93bba94e84ce863cb565ac1d054
briankod <brianmugambi199@gmail.com>
2022-09-29 20:53:38 +0300

    Web server

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,6 @@
+# Automate the task of creating a custom HTTP header response  (Using Puppet)
+
+exec { 'command':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default ; sudo service nginx restart',
+}
