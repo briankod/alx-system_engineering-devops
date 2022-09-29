5b82b1e7c55ec9df13294ccede5c6c0f390f3cc7
briankod <brianmugambi199@gmail.com>
2022-09-29 14:56:11 +0300

    Web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,5 +5,5 @@ exec {'/usr/bin/env sudo apt-get -y install nginx': }
 exec {'/usr/bin/env echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html': }
 exec {'/usr/bin/env sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
 exec {'/usr/bin/env sudo sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
-exec {'/usr/bin/env echo "Ceci n'est pas une page" | sudo tee /var/www/html/custom_404.html': }
+exec {'/usr/bin/env echo "Ceci n\'est pas une page" | sudo tee /var/www/html/custom_404.html': }
 exec {'/usr/bin/env sudo service nginx start': }
