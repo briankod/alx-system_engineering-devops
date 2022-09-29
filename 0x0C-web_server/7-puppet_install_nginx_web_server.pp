3ed2d5284c590faea1e305c233703269a0ac71c5
briankod <brianmugambi199@gmail.com>
2022-09-29 12:22:06 +0300

    Web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,22 @@
 # Configure an Ubuntu machine (Using Puppet)
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default ; sudo service nginx restart',
+package { 'nginx':
+  ensure => installed,
+  name   => 'nginx',
+}
+
+file_line { 'redirect':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World!',
+}
+
+service { 'start nginx':
+  ensure => running,
+  require => Package['nginx'],
 }
