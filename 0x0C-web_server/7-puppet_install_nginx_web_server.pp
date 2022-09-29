8240ee10cc9e16a8011c91eaf0965a8053339375
briankod <brianmugambi199@gmail.com>
2022-09-29 10:13:09 +0300

    Web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,7 +4,7 @@ exec {'/usr/bin/env sudo apt update': }
 exec {'/usr/bin/env sudo apt install nginx': }
 exec {'/usr/bin/env sudo service nginx start': }
 exec {'/usr/bin/env echo "echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html': }
-exec {'/usr/bin/env sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
 exec {'/usr/bin/env echo "Ceci n'est pas une page" | sudo tee /var/www/html/custom_404.html': }
-exec {'/usr/bin/env sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env sudo sed -i "/server_name _;/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
 exec {'/usr/bin/env sudo service nginx restart': }
