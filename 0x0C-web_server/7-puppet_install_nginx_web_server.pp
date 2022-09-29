63062514cfa5017d29d819eb7c18f80879e9d30c
briankod <brianmugambi199@gmail.com>
2022-09-29 12:40:46 +0300

    Web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,9 @@
 # Configure an Ubuntu machine (Using Puppet)
 
-package { 'nginx':
-  ensure => installed,
-  name   => 'nginx',
-}
-
-file_line { 'redirect':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-file { '/var/www/html/index.nginx-debian.html':
-  content => 'Hello World!',
-}
-
-service { 'start nginx':
-  ensure => running,
-  require => Package['nginx'],
-}
+exec {'/usr/bin/env sudo apt-get -y update': }
+exec {'/usr/bin/env sudo apt-get -y install nginx': }
+exec {'/usr/bin/env echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html': }
+exec {'/usr/bin/env sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html': }
+exec {'/usr/bin/env sudo sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env sudo service nginx start': }
