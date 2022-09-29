704cac9b29e2ccf782bdc62c320dbfa91bfa4804
briankod <brianmugambi199@gmail.com>
2022-09-29 13:15:44 +0300

    Web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,9 +1,6 @@
 # Configure an Ubuntu machine (Using Puppet)
 
-exec {'/usr/bin/env sudo apt-get -y update': }
-exec {'/usr/bin/env sudo apt-get -y install nginx': }
-exec {'/usr/bin/env echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html': }
-exec {'/usr/bin/env sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
-exec {'/usr/bin/env echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html': }
-exec {'/usr/bin/env sudo sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default': }
-exec {'/usr/bin/env sudo service nginx start': }
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
