db3175e1bb93613bfda11f2abd47bd7be8665870
briankod <brianmugambi199@gmail.com>
2022-11-05 22:15:01 +0300

    Web stack debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# correct a typo (Using Puppet)
+
+exec { 'fix':
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+/var/www/html/wp-settings.php; service apache2 restart"',
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
